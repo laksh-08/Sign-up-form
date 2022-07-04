1a2971a4412ec2e43a21830637546cccfd3565d2
laksh-08 <lakshmds1112@gmail.com>
2022-07-04 09:06:37 -1000

    Js file

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ function validatePassword() {
     let pw1 = document.getElementById("pw").value;
     let pw2 = document.getElementById("confirm-pw").value;
     if(pw1 != pw2){
-     alert('not Match');
+     alert('*passsword not Match');
     }
     else{
-     alert('match');
+     alert('');
     }
 }
